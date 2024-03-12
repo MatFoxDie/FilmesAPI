43fb8776fef9bf9ece3d2284e18dbdfc6464799b
matheus camacho <matheus_camacho27@hotmail.com>
2024-03-11 21:39:31 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,33 @@
+﻿using FilmesAPI.Validator;
+using FluentValidation.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O titulo do filme é obrigatorio")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O gênero do filme é obrigatorio")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+    [Required]
+    [Range(70,600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
+    public int Duracao { get; set; }
+    
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ FilmesAPI/Validator/FilmeValidator.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FilmesAPI.Models;
+using FluentValidation;
+
+namespace FilmesAPI.Validator
+{
+    //private FilmeValidator _filmeValidator;
+
+    //_filmeValidator = new FilmeValidator();
+
+    //var validationResult = _filmeValidator.Validate(filme);
+    //if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
+    public class FilmeValidator : AbstractValidator<Filme>
+    {
+        public FilmeValidator()
+        {
+            //Não pode ser vazio ou maior que 200 caracteres
+            RuleFor(x => x.Titulo).
+                NotEmpty().WithMessage("O título do filme é obrigatório").
+                MaximumLength(200).WithMessage("O título do filme não pode exceder 200 caracteres");
+
+            //Genero - Não pode ser vazio ou maior que 200 caracteres
+            RuleFor(x => x.Genero).
+                NotEmpty().WithMessage("O gênero do filme é obrigatório").
+                MaximumLength(20).WithMessage("O gênero do filme não pode exceder 30 caracteres");
+
+            //Não pode ser vazio, e precisa ter o valor entre 70 e 240
+            RuleFor(x => x.Duracao).
+                NotEmpty().WithMessage("A duração do filme é obrigatória").
+                GreaterThan(70).WithMessage("O valor da duração do filme deve ser maior que 70 minutos").
+                LessThan(240).WithMessage("O valor da duração do filme deve ser maior que 240 minutos");
+        }
+    }
+}
